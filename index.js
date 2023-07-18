e82f5e4a856f551e9208942de9249fc090030375
shivaganeshtechis <shivaganesh@techis.io>
2023-07-18 18:51:11 +0530

    Finale

---
+++ index.js
@@ -0,0 +1,7 @@
+// write a function to remove nesting from the Array
+// input = [1,2,3,[4,5,[6,7]], [[[[NaN], undefined]]]]
+// output - [1,2,3,4,5,6,7, NaN, undefined]
+
+// const test = [1,2,3,[4,5,[6,7]], [[[[NaN], undefined]]]]
+
+console.log("Hello, World!");
